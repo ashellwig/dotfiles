74643451de4bd40081e1c994d8ab3b8068075e93
ashellwig <hellwigashton@gmail.com>
2017-08-28 01:36:24 -0600

    updated bootstraps

--- .yadm/prestolink.zsh
+++ .yadm/prestolink.zsh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 
+mkdir ~/.zsh
+export ZDOTDIR=~/.zsh
+git clone --recursive https://github.com/ashellwig/.zprezto.git "${ZDOTDIR}/.zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR}"/.zpresto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR}/.$rcfile:t}"
