cb04b0d430619a5e3990c2c4b64e60ffc6e9e945
ashellwig <hellwigashton@gmail.com>
2017-08-28 23:22:07 -0600

    adding mutt

--- .yadm/prestolink.zsh
+++ .yadm/prestolink.zsh
@@ -1,9 +1,7 @@
 #!/usr/bin/env zsh
 
-mkdir ~/.zsh
 export ZDOTDIR="~/.zsh"
-git clone --recursive https://github.com/ashellwig/.zprezto.git "${ZDOTDIR}/.zprezto"
 setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR}"/.zpresto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR}/.$rcfile:t}"
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR}/.${rcfile:t}"
 done
