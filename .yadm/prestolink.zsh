7bdec18afde3665706fd545aa694f37359c63053
ashellwig <hellwigashton@gmail.com>
2017-08-31 22:42:01 -0600

    cleaned i3 config

--- .yadm/prestolink.zsh
+++ .yadm/prestolink.zsh
@@ -2,6 +2,6 @@
 
 setopt EXTENDED_GLOB
 export ZDOTDIR="~/.zsh"
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+for rcfile in "${ZDOTDIR}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR}/.${rcfile:t}"
 done
