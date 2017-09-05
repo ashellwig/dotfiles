d9307b6a488bb9cb9bfd8897500ac98971ff9c72
ashellwig <hellwigashton@gmail.com>
2017-09-05 00:39:14 -0600

    updating bootstrap

--- .yadm/neededfunctions.sh
+++ .yadm/neededfunctions.sh
@@ -4,7 +4,7 @@
 
 # Install Zsh if not Found
 function install_zsh_inf {
-	if "$(command -v zsh)" != "/usr/bin/zsh"; then
+	if "$(command -v zsh)" ! "/usr/bin/zsh"; then
 		echo "Please input your distribution"
 		echo "Only Arch Linux and RHEL 7.3 with yum are supported"
 		read DISTR
@@ -30,18 +30,5 @@ function install_zsh_inf {
 		echo "Zsh is already installed."
 		echo "If the config is incorrect, run \"install_prezto_yadm\"."
 		echo "Exiting."
-		exit 0
 	fi
-}
-
-
-# Install Prezto
-function install_prezto_yadm {
-	if [ "$(echo $ZDOTDIR)" != ${HOME}/.zsh  ]; then
-		export ZDOTDIR=${HOME}/.zsh
-	fi
-	git clone --recursive https://github.com/ashellwig/.zprezto.git "${ZDOTDIR}/.zprezto"
-	for rcfile in "${ZDOTDIR}"/.zprezto/runcoms/^README.md(.N); do
-		ln -s "$rcfile" "${ZDOTDIR}/.${rcfile:t}"
-	done
 }
